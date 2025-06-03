6ae7c151d8c52bebc2ec079eeb2a8be409fadd65
Benaiah Galavu <bentheaya@gmail.com>
2025-06-03 16:31:01 +0300

    User app fixed

--- nexus-backend/users/admin.py
+++ nexus-backend/users/admin.py
@@ -3,35 +3,32 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
 
-from .models import User
+from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
     """Custom User Admin for the NEXUS platform."""
-    model = User
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'points')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('username', 'first_name', 'last_name', 'email')
-    ordering = ('username',)
+    model = CustomUser
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'groups')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
     
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {
-            'fields': ('first_name', 'last_name', 'email', 'profile_picture', 'bio')
-        }),
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('username', 'first_name', 'last_name')}),
         (_('Permissions'), {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-        (_('NEXUS'), {'fields': ('points', 'email_verified')}),
     )
     
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2'),
+            'fields': ('email', 'username', 'password1', 'password2', 'first_name', 'last_name'),
         }),
     )
 
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
