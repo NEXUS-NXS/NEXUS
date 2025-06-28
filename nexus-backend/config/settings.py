77779fa7f51e77b132c163bfedf54be739d95b98
Benaiah Galavu <bentheaya@gmail.com>
2025-06-28 14:28:28 +0300

    simulation engine

--- nexus-backend/config/settings.py
+++ nexus-backend/config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'resources.apps.ResourcesConfig',
     'chat.apps.ChatConfig',
     'accounts.apps.AccountsConfig',
+    'simulations',
 ]   
 
 MIDDLEWARE = [
@@ -230,3 +231,9 @@ LOGGING = {
         },
     },
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
