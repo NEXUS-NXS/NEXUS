435eacba59919396a6a3ad1dfb1ecc5a72f5a6da
Simon Muthungu <henrymsechu1@gmail.com>
2025-06-30 12:06:42 +0300

    Pulling Jeremiahs work

--- nexus-backend/accounts/serializers.py
+++ nexus-backend/accounts/serializers.py
@@ -1,8 +1,8 @@
-# auth/serializers.py
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from .models import Profile
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     full_name = serializers.CharField(max_length=255, required=True, write_only=True)
