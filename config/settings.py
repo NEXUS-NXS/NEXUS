ee57f3b40be12c630449ac63e16ce062c326b583
j1ere <jeremywambua106@gmail.com>
2025-07-02 17:59:00 +0300

    urlupdate

--- config/settings.py
+++ config/settings.py
@@ -69,8 +69,7 @@ MIDDLEWARE = [
 # CORS settings
 # CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:5173').split(',')
 CORS_ALLOWED_ORIGINS = [
-    'https://127.0.0.1:5173',
-    'http://127.0.0.1:5173',
+    'https://eloquent-gumdrop-b7949d.netlify.app',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
